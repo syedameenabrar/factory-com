54fd5a02f46fd2e735ae019a5eea20adce925343
Ameen <syedameenabrar786@gmail.com>
2024-12-04 14:24:14 +0530

    image issue solved (optional error)

--- controllers/factory.controller.js
+++ controllers/factory.controller.js
@@ -1,28 +1,27 @@
 const factoryService = require("../services/factory.service");
-const { logger, responser, upload } = require("database-connection-function-com")
-
+const { logger, AppError, responser, upload } = require("database-connection-function-com")
 module.exports.createFactory = async (req, res) => {
     logger.info("Creating Factory Controllers");
     const reqData = req.body;
     const type = req.body.collectionName
 
-    if (reqData.mainPicture) {
-        const singlePicture = await upload.uploadOnCloudinary(reqData.mainPicture, "Factory")
-        reqData.mainPicture = singlePicture.secure_url
+    if (reqData?.mainPicture) {
+        const singlePicture = await upload.uploadOnCloudinary(reqData?.mainPicture, "Factory")
+        reqData.mainPicture = singlePicture?.secure_url
     }
 
   
-    // if (Array.isArray(reqData.arrayImages)) {
-    //     const multiPictures = await upload.uploadArrayImage(reqData.arrayImages, "Factory");
+    // if (Array.isArray(reqData?.arrayImages)) {
+    //     const multiPictures = await upload.uploadArrayImage(reqData?.arrayImages, "Factory");
 
     //     // Map the response to the desired format
-    //     reqData.arrayImages = multiPictures.map(picture => {
-    //         const secureUrl = picture.cloudinaryResponse?.secure_url;
+    //     reqData?.arrayImages = multiPictures.map(picture => {
+    //         const secureUrl = picture?.cloudinaryResponse?.secure_url;
     //         return secureUrl ? { img: secureUrl } : null;
     //     }).filter(obj => obj !== null);  // Filter out any null values
 
     //     // Handle cases where not all images may have been uploaded successfully
-    //     if (reqData.arrayImages.length === 0) {
+    //     if (reqData?.arrayImages.length === 0) {
     //         return responser.send(400, "Failed to upload any images", req, res);
     //     }
     // }
@@ -31,77 +30,94 @@ module.exports.createFactory = async (req, res) => {
     const uploadImages = async (images, folder) => {
         const multiPictures = await upload.uploadArrayImage(images, folder);
         return multiPictures
-            .map(picture => picture.cloudinaryResponse?.secure_url ? { img: picture.cloudinaryResponse.secure_url } : null)
+            .map(picture => picture?.cloudinaryResponse?.secure_url ? { img: picture?.cloudinaryResponse?.secure_url } : null)
             .filter(Boolean);
     };
 
     // Upload array images if present
-    if (Array.isArray(reqData.arrayImages)) {
+    if (Array.isArray(reqData?.arrayImages) && reqData.arrayImages.length > 0) {
         reqData.arrayImages = await uploadImages(reqData.arrayImages, "arrayImages");
-        if (!reqData.arrayImages.length) {
-            return responser.send(400, "Failed to upload array images", req, res);
+        if (reqData.arrayImages.length === 0) {
+            throw new AppError(400, "Failed to upload array images");
         }
     }
 
     // Upload vahical images if present
-    if (Array.isArray(reqData.vehiclePictures)) {
-        reqData.vehiclePictures = await uploadImages(reqData.vehiclePictures, "vehiclePictures");
-        if (!reqData.vehiclePictures.length) {
-            return responser.send(400, "Failed to upload vahical images", req, res);
+    if (Array.isArray(reqData?.vehiclePictures) && reqData.vehiclePictures.length > 0) {
+        reqData.vehiclePictures = await uploadImages(reqData?.vehiclePictures, "vehiclePictures");
+        // if (!reqData?.vehiclePictures.length) {
+            if (reqData?.vehiclePictures.length === 0) {
+
+                throw new AppError(400, "Failed to vehiclePictures images");
+
         }
     }
 
     // Upload vahical images if present
-    if (Array.isArray(reqData.chasisPictures)) {
-        reqData.chasisPictures = await uploadImages(reqData.chasisPictures, "chasisPictures");
-        if (!reqData.chasisPictures.length) {
-            return responser.send(400, "Failed to upload chasis picture images", req, res);
+    if (Array.isArray(reqData?.chasisPictures) && reqData.chasisPictures.length > 0) {
+        reqData.chasisPictures = await uploadImages(reqData?.chasisPictures, "chasisPictures");
+        // if (!reqData?.chasisPictures.length) {
+            if (reqData?.chasisPictures.length === 0) {
+                throw new AppError(400, "Failed to chasisPictures images");
+
         }
     }
 
 
     // Upload vahical images if present
-    if (Array.isArray(reqData.enginePictures)) {
-        reqData.enginePictures = await uploadImages(reqData.enginePictures, "enginePictures");
-        if (!reqData.enginePictures.length) {
-            return responser.send(400, "Failed to upload engine picture images", req, res);
+    if (Array.isArray(reqData?.enginePictures) && reqData.enginePictures.length > 0) {
+        reqData.enginePictures = await uploadImages(reqData?.enginePictures, "enginePictures");
+        // if (!reqData?.enginePictures.length) {
+            if (reqData?.enginePictures.length === 0) {
+                throw new AppError(400, "Failed to enginePictures images");
+
         }
     }
 
     // Upload vahical images if present
-    if (Array.isArray(reqData.scrapLetterPictures)) {
-        reqData.scrapLetterPictures = await uploadImages(reqData.scrapLetterPictures, "scrapLetterPictures");
-        if (!reqData.scrapLetterPictures.length) {
-            return responser.send(400, "Failed to upload scrap letter images", req, res);
+    if (Array.isArray(reqData?.scrapLetterPictures) && reqData.scrapLetterPictures.length > 0) {
+        reqData.scrapLetterPictures = await uploadImages(reqData?.scrapLetterPictures, "scrapLetterPictures");
+        // if (!reqData?.scrapLetterPictures.length) {
+            if (reqData?.scrapLetterPictures.length === 0) {
+                throw new AppError(400, "Failed to scrapLetterPictures images");
+
         }
     }
 
     // Upload vahical images if present
-    if (Array.isArray(reqData.userDocumentsPictures)) {
-        reqData.userDocumentsPictures = await uploadImages(reqData.userDocumentsPictures, "userDocumentsPictures");
-        if (!reqData.userDocumentsPictures.length) {
-            return responser.send(400, "Failed to upload user document images", req, res);
+    if (Array.isArray(reqData?.userDocumentsPictures) && reqData.userDocumentsPictures.length > 0) {
+        reqData.userDocumentsPictures = await uploadImages(reqData?.userDocumentsPictures, "userDocumentsPictures");
+        // if (!reqData?.userDocumentsPictures.length) {
+            if (reqData?.userDocumentsPictures.length === 0) {
+                throw new AppError(400, "Failed to userDocumentsPictures images");
+
         }
     }
     // Upload rcPictures images if present
-    if (Array.isArray(reqData.rcPictures)) {
-        reqData.rcPictures = await uploadImages(reqData.rcPictures, "rcPictures");
-        if (!reqData.rcPictures.length) {
-            return responser.send(400, "Failed to upload rcPictures images", req, res);
+    if (Array.isArray(reqData?.rcPictures) && reqData.rcPictures.length > 0) {
+        reqData.rcPictures = await uploadImages(reqData?.rcPictures, "rcPictures");
+        // if (!reqData?.rcPictures.length) {
+            if (reqData?.rcPictures.length === 0) {
+                throw new AppError(400, "Failed to rcPictures images");
+
         }
     }
     // Upload towingPictures images if present
-    if (Array.isArray(reqData.towingPictures)) {
-        reqData.towingPictures = await uploadImages(reqData.towingPictures, "towingPictures");
-        if (!reqData.towingPictures.length) {
-            return responser.send(400, "Failed to upload towingPictures images", req, res);
+    if (Array.isArray(reqData?.towingPictures) && reqData.towingPictures.length > 0) {
+        reqData.towingPictures = await uploadImages(reqData?.towingPictures, "towingPictures");
+        // if (!reqData?.towingPictures.length) {
+            if (reqData?.towingPictures.length === 0) {
+                throw new AppError(400, "Failed to towingPictures images");
+
         }
     }
     // Upload scrapPictures images if present
-    if (Array.isArray(reqData.scrapPictures)) {
-        reqData.scrapPictures = await uploadImages(reqData.scrapPictures, "scrapPictures");
-        if (!reqData.scrapPictures.length) {
-            return responser.send(400, "Failed to upload scrapPictures images", req, res);
+    if (Array.isArray(reqData?.scrapPictures) && reqData.scrapPictures.length > 0) {
+        reqData.scrapPictures = await uploadImages(reqData?.scrapPictures, "scrapPictures");
+        // if (!reqData?.scrapPictures.length) {
+            if (reqData?.scrapPictures.length === 0) {
+                throw new AppError(400, "Failed to scrapPictures images");
+
         }
     }
 
@@ -126,7 +142,7 @@ module.exports.getOneFactory = async (req, res) => {
     const reqData = req.body;
     const type = req.body.collectionName
     const condition = {
-        _id: reqData.id
+        _id: reqData?.id
     }
     const data = await factoryService.getOneFactory(type, condition)
     logger.data("data", data);
@@ -142,91 +158,92 @@ module.exports.updateFactory = async (req, res) => {
         _id: params.id
     }
 
-    if (Array.isArray(reqData.chasisPictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.chasisPictures, "chasisPictures");
+    if (Array.isArray(reqData?.chasisPictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.chasisPictures, "chasisPictures");
 
         // Map the response to the desired format
         reqData.chasisPictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
 
+    if (reqData?.mainPicture) {
+        const singlePicture = await upload.uploadOnCloudinary(reqData?.mainPicture, "Factory")
+        reqData.mainPicture = singlePicture?.secure_url
+    }
 
     
     
-    
-    
-    
-    if (Array.isArray(reqData.arrayImages)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.arrayImages, "arrayImages");
+    if (Array.isArray(reqData?.arrayImages)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.arrayImages, "arrayImages");
 
         // Map the response to the desired format
         reqData.arrayImages = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.vehiclePictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.vehiclePictures, "vehiclePictures");
+    if (Array.isArray(reqData?.vehiclePictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.vehiclePictures, "vehiclePictures");
 
         // Map the response to the desired format
         reqData.vehiclePictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.enginePictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.enginePictures, "enginePictures");
+    if (Array.isArray(reqData?.enginePictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.enginePictures, "enginePictures");
 
         // Map the response to the desired format
         reqData.enginePictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.userDocumentsPictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.userDocumentsPictures, "userDocumentsPictures");
+    if (Array.isArray(reqData?.userDocumentsPictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.userDocumentsPictures, "userDocumentsPictures");
 
         // Map the response to the desired format
         reqData.userDocumentsPictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.scrapLetterPictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.scrapLetterPictures, "scrapLetterPictures");
+    if (Array.isArray(reqData?.scrapLetterPictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.scrapLetterPictures, "scrapLetterPictures");
 
         // Map the response to the desired format
         reqData.scrapLetterPictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.rcPictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.rcPictures, "rcPictures");
+    if (Array.isArray(reqData?.rcPictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.rcPictures, "rcPictures");
 
         // Map the response to the desired format
         reqData.rcPictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.towingPictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.towingPictures, "towingPictures");
+    if (Array.isArray(reqData?.towingPictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.towingPictures, "towingPictures");
 
         // Map the response to the desired format
         reqData.towingPictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
-    if (Array.isArray(reqData.scrapPictures)) {
-        const multiPictures = await upload.uploadArrayImage(reqData.scrapPictures, "scrapPictures");
+    if (Array.isArray(reqData?.scrapPictures)) {
+        const multiPictures = await upload.uploadArrayImage(reqData?.scrapPictures, "scrapPictures");
 
         // Map the response to the desired format
         reqData.scrapPictures = multiPictures.map(picture => {
-            const secureUrl = picture.cloudinaryResponse?.secure_url;
+            const secureUrl = picture?.cloudinaryResponse?.secure_url;
             return secureUrl ? { img: secureUrl } : null;
         }).filter(obj => obj !== null);  // Filter out any null values
     }
